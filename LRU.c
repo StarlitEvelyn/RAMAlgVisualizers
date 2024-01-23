eaa5541edf435658cdadffad1fe31d1a54d2f14d
Starlight <StarlightWT@protonmail.com>
2024-01-23 09:37:27 +0100

    Added LRU. Made program stop after X steps, renamed RAM to PAG(E)

--- LRU.c
+++ LRU.c
@@ -3,23 +3,27 @@
 #include <stdlib.h>
 #include <windows.h>
 
-#define AMOUNT 5 // Amount of numbers you want <0; AMOUNT>
-#define RAMS 3   // How many ram slots you
-#define COLUMNS 20
-#define SPEED 1 // prints per second
+#define AMOUNT 10  // Amount of numbers you want <0; AMOUNT)
+                   //! >10 breaks UI, do not use with this version!
+#define RAMS 3     // How many pages
+#define COLUMNS 30 // Maximum amount of columns
+#define SPEED 1    // prints per second
+#define STEP 5     // 0 = continous, >0 = how many new requests to make before pause
 
 void printTable(int[RAMS][COLUMNS], int);
 void removeFirstColumn(int[RAMS][COLUMNS]);
 void addColumn(int[RAMS][COLUMNS]);
-void changeSpot(int[RAMS][COLUMNS], int);
+void changeSpot(int[RAMS][COLUMNS], int, int);
 int findInRam(int[RAMS][COLUMNS], int);
+int getLatest(int[RAMS][COLUMNS]);
 int timeout(int seconds);
+float changesCount();
 void moveRequestsLeft();
 void printRequests();
 
 int columnCount = 0;
 int rowCount = 0;
-int request = 1, lastRequest;
+int request = 1, lastRequest = 0;
 int requests[COLUMNS] = {};
 int changes[RAMS][COLUMNS] = {};
 int main()
@@ -27,11 +31,19 @@ int main()
     srand(time(NULL));
 
     int table[RAMS][COLUMNS] = {};
+    int latest;
+    int startFill = 0, stepCount = 0, sameRequests = 0;
     while (1)
     {
         request = rand() % AMOUNT;
         if (lastRequest == request)
+            sameRequests++;
+        if (sameRequests > 2 && lastRequest == request)
+        {
             continue;
+            sameRequests = 0;
+        }
+        lastRequest = request;
         if (columnCount == COLUMNS)
         {
             removeFirstColumn(table);
@@ -44,13 +56,27 @@ int main()
         addColumn(table);
         system("cls");
 
-        if (!findInRam(table, request))
-            changeSpot(table, request);
+        latest = getLatest(table);
+
+        if (startFill < RAMS && !findInRam(table, request))
+        {
+            changeSpot(table, request, startFill);
+            startFill++;
+        }
+        else if (!findInRam(table, request))
+            changeSpot(table, request, latest);
 
         printRequests();
         printTable(table, request);
+        printf("Avg. changes: %.2f\n", changesCount() / COLUMNS);
         printf("Yellow - Changes\nPurple - Highlight for REQ in RAM\n");
         timeout(SPEED);
+        stepCount++;
+        if (STEP != 0 && stepCount >= STEP)
+        {
+            system("pause");
+            stepCount = 0;
+        }
     }
 }
 
@@ -58,7 +84,7 @@ void printTable(int table[RAMS][COLUMNS], int req)
 {
     for (int i = 0; i < RAMS; i++)
     {
-        printf("RAM%d", i);
+        printf("PAG%d", i);
         for (int j = 0; j < columnCount; j++)
         {
             if (table[i][j] == request && j == columnCount - 1)
@@ -102,14 +128,14 @@ void addColumn(int table[RAMS][COLUMNS])
     columnCount += 1;
 }
 
-void changeSpot(int table[RAMS][COLUMNS], int newValue)
+void changeSpot(int table[RAMS][COLUMNS], int newValue, int oldestIndex)
 {
-    table[rowCount][columnCount - 1] = newValue;
+    table[oldestIndex][columnCount - 1] = newValue;
     for (int i = 0; i < RAMS; i++)
     {
         changes[i][columnCount - 1] = 0;
     }
-    changes[rowCount][columnCount - 1] = 1;
+    changes[oldestIndex][columnCount - 1] = 1;
     rowCount++;
     if (rowCount >= RAMS)
         rowCount = 0;
@@ -151,4 +177,47 @@ void moveRequestsLeft()
     {
         requests[i] = requests[i + 1];
     }
+}
+
+int getLatest(int table[RAMS][COLUMNS])
+{
+    int currentRam[RAMS] = {};
+    int latestReads[RAMS] = {};
+    int oldest;
+    for (int i = 0; i < RAMS; i++)
+    {
+        currentRam[i] = table[i][columnCount - 1];
+    }
+
+    for (int i = 0; i < columnCount; i++)
+    {
+        for (int j = 0; j < RAMS; j++)
+        {
+            if (requests[i] == currentRam[j])
+                latestReads[j] = i;
+        }
+    }
+
+    oldest = 0;
+    for (int i = 0; i < RAMS; i++)
+    {
+        if (latestReads[oldest] > latestReads[i])
+            oldest = i;
+    }
+
+    return oldest;
+}
+
+float changesCount()
+{
+    int changeCount = 0;
+    for (int i = 0; i < RAMS; i++)
+    {
+        for (int j = 0; j < COLUMNS; j++)
+        {
+            if (changes[i][j] == 1)
+                changeCount++;
+        }
+    }
+    return changeCount;
 }
